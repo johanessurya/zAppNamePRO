845e5211453bb4b0d0e97cf70d98910502cce577
Johanes Surya <johanes.surya43@gmail.com>
2016-01-22 20:46:12 +0700

    Add Register Controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,9 +23,7 @@ Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/register', function () {
-    return view('register');
-});
+Route::get('/register', 'RegisterController@index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
