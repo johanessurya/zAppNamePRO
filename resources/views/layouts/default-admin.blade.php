2fdb9e346ed553f3c262ee7be1c8921413634e0d
Johanes Surya <johanes.surya43@gmail.com>
2016-01-28 09:45:34 +0700

    Add change password in general settings

--- resources/views/layouts/default-admin.blade.php
+++ resources/views/layouts/default-admin.blade.php
@@ -275,55 +275,14 @@
               <h3 class="control-sidebar-heading">General Settings</h3>
               <div class="form-group">
                 <label class="control-sidebar-subheading">
-                  Report panel usage
-                  <input type="checkbox" class="pull-right" checked>
+                  Change Password
                 </label>
-                <p>
-                  Some information about this general settings option
-                </p>
+                <input class="form-control input-sm" type="password" placeholder="Old Password" name="old_password">
+                <input class="form-control input-sm" type="password" placeholder="New Password">
+                <input class="form-control input-sm" type="password" placeholder="Retype New Password">
               </div><!-- /.form-group -->
-
               <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Allow mail redirect
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-                <p>
-                  Other sets of options are available
-                </p>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Expose author name in posts
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-                <p>
-                  Allow the user to show his name in blog posts
-                </p>
-              </div><!-- /.form-group -->
-
-              <h3 class="control-sidebar-heading">Chat Settings</h3>
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Show me as online
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Turn off notifications
-                  <input type="checkbox" class="pull-right">
-                </label>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Delete chat history
-                  <a href="javascript::;" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
-                </label>
+                <button type="submit" class="btn btn-primary pull-right">Change</button>
               </div><!-- /.form-group -->
             </form>
           </div><!-- /.tab-pane -->
