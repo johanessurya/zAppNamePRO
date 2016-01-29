5d7923661c045953b26ddf4a7efb96999f2f587f
Johanes Surya <johanes.surya43@gmail.com>
2016-01-29 09:36:05 +0700

    Remove border on top of create event pop up modal

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -17,7 +17,7 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <form id="quicksave-form-body">
-          <div class="modal-header">
+          <div class="modal-header remove-border-bottom">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <h4 class="modal-title"></h4>
           </div>
--- resources/views/layouts/default-admin.blade.php
+++ resources/views/layouts/default-admin.blade.php
@@ -22,6 +22,8 @@
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="{{ asset('dist/css/skins/_all-skins.min.css')}}">
+    <!-- Custom Style for Steve -->
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
