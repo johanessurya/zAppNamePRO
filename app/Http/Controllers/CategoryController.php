3249e7fe990eafce8f2805f340716a4fb79a3554
Johanes Surya <johanes.surya43@gmail.com>
2016-01-29 12:57:18 +0700

    Missing category controller to handle response all categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use DB;
+use Auth;
+use App\Category;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    /*
+    Get category list
+    - Show category with no company ID
+    - Show category associated with company id that associated with user id
+    */
+    public function getSubCategory($categoryId) {
+      $rows = DB::table('subcategory')->where('category_id', $categoryId)->get();
+
+      return $rows;
+    }
+}
