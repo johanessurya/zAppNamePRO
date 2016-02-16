63671296c8fef115fd15e287022df1b67c4dce64
Johanes Surya <johanes.surya43@gmail.com>
2016-02-16 20:31:15 +0700

    Correct Toolbar

--- public/js/ckeditor-config.js
+++ public/js/ckeditor-config.js
@@ -1,6 +1,9 @@
 // Configure CKEditor Toolbar
 CKEDITOR.editorConfig = function(config) {
   config.toolbar = [
-    { name: 'basicstyles', items: ['Bold', 'Italic', 'Strike'] }
+    { name: 'basicstyles', items: ['Bold', 'Italic', 'Strike'] },
+    { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent'] },
+    { name: 'links', items: ['Link', 'Unlink'] },
+    { name: 'about', items: ['About'] }
   ];
 };
