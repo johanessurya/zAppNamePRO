cd47de3eb79db0a400b04915ce8eaeb9999abd05
Johanes Surya <johanes.surya43@gmail.com>
2016-02-16 22:20:01 +0700

    Remove minimize and close button

--- resources/views/dashboard/activitylog.blade.php
+++ resources/views/dashboard/activitylog.blade.php
@@ -158,10 +158,6 @@
     <div class="box box-info">
       <div class="box-header with-border">
         <h3 class="box-title">Activity Log: <strong>January 1, 2016 - January 30, 2016</strong></h3>
-        <div class="box-tools pull-right">
-          <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-          <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-        </div>
       </div><!-- /.box-header -->
       <div class="box-body">
         <div class="table-responsive">
