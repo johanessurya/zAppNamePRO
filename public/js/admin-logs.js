7089d4bd71325984c79ecb2db36a04070790ccec
Johanes Surya <johanes.surya43@gmail.com>
2016-03-01 08:08:38 +0700

    Fix chart hover error

--- public/js/admin-logs.js
+++ public/js/admin-logs.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Init pie chart
-  // $global.initPieChart();
+  $global.initPieChart();
 
   // Init CKEditor
   $global.initCKEditor();
--- public/js/admin.js
+++ public/js/admin.js
@@ -185,42 +185,55 @@ $global.reloadPieChart = function() {
     }).done(function(data) {
       $global.reloadPieChartlegend(data);
 
-      //-------------
-      //- PIE CHART (Report)-
-      //-------------
-      // Get context with jQuery - using jQuery's .get() method.
-      var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
-      var pieChart = new Chart(pieChartCanvas);
-      var PieData = data;
-
-      var pieOptions = {
-        //Boolean - Whether we should show a stroke on each segment
-        segmentShowStroke: true,
-        //String - The colour of each segment stroke
-        segmentStrokeColor: "#fff",
-        //Number - The width of each segment stroke
-        segmentStrokeWidth: 2,
-        //Number - The percentage of the chart that we cut out of the middle
-        percentageInnerCutout: 50, // This is 0 for Pie charts
-        //Number - Amount of animation steps
-        animationSteps: 100,
-        //String - Animation easing effect
-        animationEasing: "easeOutBounce",
-        //Boolean - Whether we animate the rotation of the Doughnut
-        animateRotate: true,
-        //Boolean - Whether we animate scaling the Doughnut from the centre
-        animateScale: false,
-        //Boolean - whether to make the chart responsive to window resizing
-        responsive: true,
-        // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-        maintainAspectRatio: true,
-        //String - A legend template
-        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+      // Remove all the segments
+      var chart = $global.pieChart;
+      while (chart.segments.length) {
+          chart.removeData();
       };
-      //Create pie or douhnut chart
-      // You can switch between pie and douhnut using the method below.
-      pieChart.Doughnut(PieData, pieOptions);
+
+      // Add all data
+      for(var i = 0; i < data.length; i++) {
+        $global.pieChart.addData(data[i]);
+      }
     });
+};
+
+$global.initPieChart = function() {
+  //-------------
+  //- PIE CHART (Report)-
+  //-------------
+  // Get context with jQuery - using jQuery's .get() method.
+  var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+  var pieChart = new Chart(pieChartCanvas);
+
+  var pieOptions = {
+    //Boolean - Whether we should show a stroke on each segment
+    segmentShowStroke: true,
+    //String - The colour of each segment stroke
+    segmentStrokeColor: "#fff",
+    //Number - The width of each segment stroke
+    segmentStrokeWidth: 2,
+    //Number - The percentage of the chart that we cut out of the middle
+    percentageInnerCutout: 50, // This is 0 for Pie charts
+    //Number - Amount of animation steps
+    animationSteps: 100,
+    //String - Animation easing effect
+    animationEasing: "easeOutBounce",
+    //Boolean - Whether we animate the rotation of the Doughnut
+    animateRotate: true,
+    //Boolean - Whether we animate scaling the Doughnut from the centre
+    animateScale: false,
+    //Boolean - whether to make the chart responsive to window resizing
+    responsive: true,
+    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+    maintainAspectRatio: true,
+    //String - A legend template
+    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+  };
+
+  // Create pie or douhnut chart
+  // You can switch between pie and douhnut using the method below.
+  $global.pieChart = pieChart.Doughnut([], pieOptions);
 };
 
 // Save and get config
