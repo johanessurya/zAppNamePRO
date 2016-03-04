353dd70954b418121e85f95ed78dd5173624ae22
Johanes Surya <johanes.surya43@gmail.com>
2016-03-04 22:43:36 +0700

    Change data table title

--- public/js/admin-logs.js
+++ public/js/admin-logs.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
       case 'table':
         if($scope.i.hasClass('fa-plus')) {
           // fa-plus
-          $('#table-title').html('Event Found');
+          $('#table-title').html('Activity Found');
         } else {
           // fa-minus
           $('#table-title').html('Data');
